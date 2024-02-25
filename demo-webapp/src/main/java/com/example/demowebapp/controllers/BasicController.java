2ba328efbab695c4855ae502ef026d66b031812f
Roberto Inconis <roberto.inconis@outlook.com>
2024-02-26 00:19:05 +0100

    exercise5

---
+++ demo-webapp/src/main/java/com/example/demowebapp/controllers/BasicController.java
@@ -0,0 +1,21 @@
+package com.example.demowebapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/")
+public class BasicController {
+
+    @Autowired
+    private Environment environment;
+    @GetMapping
+    public String devName(){
+        return environment.getProperty("devName");
+    }
+}
